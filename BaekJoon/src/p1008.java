bd1aaee2a69df6ab262d92a6e9e0408b8b7cc9cb
minsung Joh <jms5049@naver.com>
2018-05-17 01:08:21 +0900

    p1008

---
+++ BaekJoon/src/p1008.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class p1008 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println((double)a/b);
+    }
+
+}
