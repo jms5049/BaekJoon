a0df93ffce97dfd91bb62473fc5d8867c37905ae
minsung Joh <jms5049@naver.com>
2018-05-17 00:27:47 +0900

    p10998

---
+++ BaekJoon/src/p10998.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class p10998 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        System.out.println(a*b);
+
+    }
+
+}
