f0174cd33daf1cf591ab83a9da5e293ee4ffe181
minsung Joh <jms5049@naver.com>
2018-05-17 01:08:52 +0900

    p10869

---
+++ BaekJoon/src/p10869.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class p10869 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+
+}
