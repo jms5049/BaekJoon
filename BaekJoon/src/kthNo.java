ebd6e6459da364a3d09d6cca86965c6d82ce9c8a
minsung Joh <jms5049@naver.com>
2018-08-25 12:04:22 +0900

    programmers-kthNo01

---
+++ BaekJoon/src/kthNo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class kthNo {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3},{4, 4, 1},{1, 7, 3}};
+        System.out.println(solution(array, commands));
+
+    }
+    
+}
